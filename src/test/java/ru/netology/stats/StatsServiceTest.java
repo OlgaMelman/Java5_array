31b92b13fe24d152ca924b59e5286eab1ce56e9a
Olga Melman <olga.melman198237@gmail.com>
2022-08-10 20:43:19 +0300

    Second commit + tests

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -22,7 +22,7 @@ public class StatsServiceTest {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        long expectedMidSale = 12;
+        long expectedMidSale = 15;
         long actualMidSale = service.midSumSale(sales);
 
         Assertions.assertEquals(expectedMidSale, actualMidSale);
